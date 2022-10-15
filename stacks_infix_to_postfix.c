d6c0dc818faca44c65190c80c8945725aa6cf74d
nahalroshan <96647004+nahalroshan@users.noreply.github.com>
2022-10-15 16:04:31 +0530

    Updated stacks_infix_to_postfix.c
    Edited the Main Function

--- stacks_infix_to_postfix.c
+++ stacks_infix_to_postfix.c
@@ -27,3 +27,68 @@ int isPriority(char c) {
   if (c == '^')
     return 3;
 }
+int main(void) {
+  int small = 0, num1, num2;
+  char str[100], *e, a, s[100];
+  printf("Enter the expression \n");
+  scanf("%s", str);
+  e = str;
+  // push('(');
+  while (*e != '\0') {
+    if (isalphanum(*e))
+      s[small++] = *e; // printf("%c", *e);
+    else if (*e == '(')
+      push(*e);
+    else if (*e == ')')
+      while ((a = pop()) != '(')
+        s[small++] = a; // printf("%c", a);
+    else {
+      while (isPriority(stack[top]) >= isPriority(*e))
+        s[small++] = pop(); // printf("%c", pop());
+      push(*e);
+    }
+    e++;
+  }
+  while (top != -1)
+    s[small++] = pop(); // printf("%c", pop());
+  s[small] = '\0';
+  printf("%s", s);
+  printf("\n");
+  e = s;
+  top = -1;
+  while (*e != '\0') {
+    if (isdigit(*e)) // can use the prebuilt function isdigit() in <ctype.h>
+                     // header file.
+      push(*e -
+           '0'); // convert the convert the character digits to actual integers
+                 // i.e ASCII value of'0' is 48 and the next digit 1 has asci
+                 // value has 49. 49-48 =1 which is the integer 1.
+    else {
+      num2 = pop();
+      num1 = pop();
+      switch (*e) {
+
+      case '+':
+        push(num1 + num2);
+        break;
+      case '-':
+        push(num1 - num2);
+
+        break;
+      case '*':
+        push(num1 * num2);
+
+        break;
+      case '/':
+        push(num1 / num2);
+
+        break;
+      }
+    }
+    e++;
+  }
+
+  printf("%d", pop());
+
+  return 0;
+}
